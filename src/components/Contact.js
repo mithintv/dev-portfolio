a8d569e81e721ef9d54ae1ed491a26e63969ee1e
Mithin <mithingthomas@gmail.com>
2022-08-26 21:38:04 -0400

    add form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef, useReducer } from "react";
 
 import Header from "../interface/Header";
 import Button from '../interface/Button';
@@ -7,52 +7,203 @@ import classes from "../sass/modules/Contact.module.scss";
 
 import emailjs from '@emailjs/browser';
 
+const contactReducer = (state, action) => {
+  switch (action.type) {
+    case 'blur':
+      return {
+        ...state,
+        [action.input.name]: {
+          ...state[action.input.name],
+          touched: action.input.touched,
+          valid: action.input.valid,
+          showError: action.input.touched && !action.input.valid
+        }
+      };
+    case 'change':
+      return {
+        ...state,
+        [action.input.name]: {
+          value: action.input.value,
+          touched: action.input.touched,
+          valid: action.input.valid,
+          showError: action.input.touched && !action.input.valid
+        }
+      };
+    case 'submit':
+      return {
+        ...state,
+        name: {
+          ...state.name,
+          touched: true,
+          showError: true && !state.name.valid
+        },
+        email: {
+          ...state.email,
+          touched: true,
+          showError: true && !state.email.valid
+        },
+        message: {
+          ...state.message,
+          touched: true,
+          showError: true && !state.message.valid
+        },
+      };
+    default:
+      return state;
+  }
+};
+
 const Contact = () => {
+  const [submit, setSubmit] = useState(false);
+  const [buttonValid, setButtonValid] = useState(true);
+  const [valid, dispatchValid] = useReducer(contactReducer, {
+    name: {
+      value: '',
+      touched: false,
+      valid: false,
+      showError: false
+    },
+    email: {
+      value: '',
+      touched: false,
+      valid: false,
+      showError: false
+    },
+    message: {
+      value: '',
+      touched: false,
+      valid: false,
+      showError: false
+    }
+  });
+  const formRef = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
-  const [submit, setSubmit] = useState(true);
-  const form = useRef();
+  // useEffect(() => {
+  //   setFormValid(prevState => {
+  //     return valid.name.valid && valid.email.valid && valid.message.valid;
+  //   });
+  // }, [valid.name.valid, valid.email.valid, valid.message.valid]);
 
-  const clickHandler = () => {
-    setSubmit(false);
+  const isValid = (inputRef) => {
+    if (inputRef.current.name === 'email') {
+      const isNotEmpty = inputRef.current.value.trim() !== '';
+      const emailValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(inputRef.current.value);
+      return isNotEmpty && emailValid;
+    }
+    return inputRef.current.value.trim() !== '';
   };
+
+  const handleBlur = (inputRef, event) => {
+    dispatchValid(
+      {
+        type: 'blur',
+        input: {
+          name: inputRef.current.name,
+          touched: true,
+          valid: isValid(inputRef)
+        }
+      });
+  };
+
+  const handleChange = (inputRef, event) => {
+    dispatchValid(
+      {
+        type: 'change',
+        input: {
+          name: inputRef.current.name,
+          value: event.target.value,
+          touched: true,
+          valid: isValid(inputRef)
+        }
+      });
+    if (formIsValid) {
+      setButtonValid(prevState => true);
+    }
+    else setButtonValid(prevState => false);
+  };
+
+  let formIsValid = false;
+  if (valid.name.valid && valid.email.valid && valid.message.valid) {
+    formIsValid = true;
+  }
 
   const submitHandler = event => {
     event.preventDefault();
-
-    emailjs.sendForm('service_72mo8y7', 'contact_form', form.current, '-YTmwUFLJVqxXgeZh')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    if (!formIsValid) {
+      setButtonValid(false);
+      dispatchValid(
+        {
+          type: 'submit',
+          input: {
+            name: 'button',
+            touched: true,
+          }
+        });
+      return;
+    } else {
+      // emailjs.sendForm('service_72mo8y7', 'contact_form', formRef.current, '-YTmwUFLJVqxXgeZh')
+      //   .then((result) => {
+      //     console.log(result.text);
+      //   }, (error) => {
+      //     console.log(error.text);
+      //   });
+    }
     setSubmit(true);
+    formIsValid = false;
   };
 
-  const content = submit ?
-    <div className={classes.intro}>
-      <p>Thank you for your sending a message! I really appreciate that you took the time to peruse my website and reach out. I will get back to you via email as soon as possible. Cheers!</p>
-      <Button size='md' onClick={clickHandler}>SEND ANOTHER MESSAGE</Button>
-    </div>
-    :
+  const sendAnotherHandler = () => {
+    setSubmit(false);
+  };
+
+  const content =
     <div className={classes.body}>
-      <form onSubmit={submitHandler} ref={form} className={classes.form}>
+      <form onSubmit={submitHandler} ref={formRef} className={submit ? classes.formHide : classes.form}>
         <div className={classes.formHeader}>
-          <input
-            name='name'
-            type='text' placeholder='Your Name' />
-          <input
-            name='email'
-            type='email' placeholder='Your Email' />
+          <div className={classes.inputWrapper}>
+            <input
+              value={valid.name.value}
+              onChange={handleChange.bind(this, nameRef)}
+              onBlur={handleBlur.bind(this, nameRef)}
+              ref={nameRef}
+              name='name'
+              type='text' placeholder='Your Name' />
+            <p className={valid.name.showError ? classes.error : ''}>Please enter a valid name</p>
+          </div>
+          <div className={classes.inputWrapper}>
+            <input
+              value={valid.email.value}
+              onChange={handleChange.bind(this, emailRef)}
+              onBlur={handleBlur.bind(this, emailRef)}
+              ref={emailRef}
+              name='email'
+              type='email' placeholder='Your Email' />
+            <p className={valid.email.showError ? classes.error : ''}>Please enter a valid email</p>
+          </div>
         </div>
         <div className={classes.formMessage}>
           <textarea
+            value={valid.message.value}
+            onChange={handleChange.bind(this, messageRef)}
+            onBlur={handleBlur.bind(this, messageRef)}
+            ref={messageRef}
             name='message'
             placeholder='Your Message' />
+          <p className={valid.message.showError ? classes.error : ''}>Message cannot be empty</p>
         </div>
-        <Button size='xl'>
+        <Button
+          disabled={!buttonValid}
+          size='xl'>
           SEND MESSAGE <i className="fa-solid fa-paper-plane"></i>
         </Button>
       </form>
+      <div className={submit ? classes.intro : classes.introHide}>
+        <p>Thank you for your sending a message! I really appreciate that you took the time to peruse my website and reach out. I will get back to you via email as soon as possible. Cheers!</p>
+        <Button size='md' onClick={sendAnotherHandler}>SEND ANOTHER MESSAGE</Button>
+      </div>
     </div>;
 
   return (
--- src/interface/Button.js
+++ src/interface/Button.js
@@ -4,6 +4,7 @@ const Button = (props) => {
 
   return (
     <button
+      disabled={props.disabled}
       type={props.type ? props.type : 'submit'}
       className={props.size ? props.size : 'primary'}
       onClick={props.onClick}>{props.children}
