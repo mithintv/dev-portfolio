faefdc95519fe44051041b07acfe20c9cd805e20
Mithin <mithingthomas@gmail.com>
2022-08-29 13:04:36 -0400

    portfolio-react description fix

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -20,10 +20,10 @@ const Portfolio = () => {
             git='https://github.com/mithintv/wordle-tracker'
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fportfolio-screen.png?alt=media&token=348fad2b-284b-4c2c-800d-8a5e98ec8a46'
+            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fportfolio-screen.png?alt=media&token=1e56e6d4-b07a-46a4-8359-e5ad66b29189'
             title='Portfolio'
-            description='My latest portfolio iteration. This is a single-page built with React and PostgreSQL.'
-            tech={['React', 'CSS3/Sass', 'PostgreSQL']}
+            description='The latest iteration of my portfolio. This is a single-page built with React, CSS3/Sass and Firebase.'
+            tech={['React', 'CSS3/Sass', 'Firebase']}
             git='https://github.com/mithintv/portfolio-react'
             link='https://portfolio-react-2022-v1.netlify.app/'
           />
@@ -31,14 +31,14 @@ const Portfolio = () => {
             img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Ffood-order-app-screen.png?alt=media&token=7c3b85d2-af31-4a6f-8ae7-2d50eaffc328'
             title='Food Order App'
             description='A front-end proof of concept with a dynamic cart for a hypothetical food ordering app built with React and Firebase.'
-            tech={['React', 'CSS3', 'Firebase']}
+            tech={['React', 'CSS3', 'Firebase', 'Netlify']}
             git='https://github.com/mithintv/food-order-app'
           />
           <Card
             img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fkeeper-app-screen.png?alt=media&token=03f8b193-b2b2-4997-aba0-ed62ca110426'
             title='Keeper'
             description='A clone of Google Keep. Create, update, and delete notes!'
-            tech={['React', 'CSS3']}
+            tech={['React', 'CSS3', 'Netlify']}
             git='https://github.com/mithintv/keeper-app'
             link='https://keeper-app-project.netlify.app/'
           />
@@ -54,7 +54,7 @@ const Portfolio = () => {
             img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fsimon-game-screen.png?alt=media&token=1d36f31e-7639-413c-b6d6-dbdc745705f8'
             title='Simon Game'
             description='This is a web implementation of Simon, an electronic game of short-term memory skill originally released in 1978 built with vanilla JavaScript and CSS3.'
-            tech={['HTML5', 'CSS3', 'JavaScript']}
+            tech={['HTML5', 'CSS3', 'JavaScript', 'GitPages']}
             git='https://github.com/mithintv/simon-game'
             link='https://mithintv.github.io/simon-game/'
           />
