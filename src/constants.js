584915af5827b29e7e54fc2f4bccd2f8aea4e3cb
Mithin <mithingthomas@gmail.com>
2023-02-23 00:05:06 -0500

    resume update

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const resumeLink = 'https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/resumes%2FMithinGThomasResume.pdf?alt=media&token=da72b0b5-f0ea-40ca-af57-16e1d820911a';
+export const resumeLink = 'https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/resumes%2FMithinGThomasResume.pdf?alt=media&token=d931b7b1-3b20-49a6-a6ec-c4184424552c';
