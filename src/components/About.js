76008ce1f461c507b6679c5869bbb341717bbc0c
Mithin <13951154+mithintv@users.noreply.github.com>
2023-03-14 20:11:38 -0400

    add more tech and horse-race

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { resumeLink } from "../constants";
 
-import Button from '../interface/Button';
-import Header from '../interface/Header';
-import classes from './About.module.scss';
+import Button from "../interface/Button";
+import Header from "../interface/Header";
+import classes from "./About.module.scss";
 
 const About = () => {
   const [tablet, setTablet] = useState(false);
@@ -12,20 +12,18 @@ const About = () => {
   const handleResize = () => {
     if (window.innerWidth <= 1024) {
       setLaptop(true);
-    }
-    else if (window.innerWidth <= 768) {
+    } else if (window.innerWidth <= 768) {
       setTablet(true);
-    }
-    else {
+    } else {
       setLaptop(false);
       setTablet(false);
     }
   };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -34,16 +32,31 @@ const About = () => {
   });
 
   return (
-    <section id='about'>
+    <section id="about">
       <div className={classes.about}>
         <Header>ABOUT</Header>
         <div className={classes.body}>
           <div className={classes.background}>
             <h3>Background</h3>
-            <p className={classes.text}>I am currently a freelance Software Engineer splitting my time between client work and personal projects. I am passionate about making the world a more egalitarian place and frequently think about ideas and solutions which hopes to make that come to fruition.</p>
-            <p className={classes.text}>When I am not coding, I am either writing a new screenplay or actively working on a film/commerical set. You can check out my film and production work <a href="https://mithin.tv">here</a>.</p>
-            {!tablet && !laptop && <a target="_blank" rel="noreferrer" href={resumeLink}><Button
-              size='md'>DOWNLOAD RESUME <i className="fa-solid fa-file-arrow-down"></i></Button></a>}
+            <p className={classes.text}>
+              I am currently a freelance Software Engineer splitting my time
+              between client work and personal projects. I am passionate about
+              making the world a more egalitarian place and frequently think
+              about ideas and solutions that can make that world a reality.
+            </p>
+            <p className={classes.text}>
+              When I am not coding, I am either writing a new screenplay or
+              actively working on a film/commerical set. You can check out my
+              film and production work <a href="https://mithin.tv">here</a>.
+            </p>
+            {!tablet && !laptop && (
+              <a target="_blank" rel="noreferrer" href={resumeLink}>
+                <Button size="md">
+                  DOWNLOAD RESUME{" "}
+                  <i className="fa-solid fa-file-arrow-down"></i>
+                </Button>
+              </a>
+            )}
           </div>
           <div className={classes.skills}>
             <h3>Technologies</h3>
@@ -52,13 +65,17 @@ const About = () => {
               <p>Cypress</p>
               <p>Bootstrap</p>
               <p>Bulma</p>
+              <p>C#</p>
+              <p>C++</p>
               <p>CSS/Sass</p>
               <p>Django</p>
               <p>EJS</p>
+              <p>Emotion</p>
               <p>Express.js</p>
               <p>Firebase</p>
               <p>Git/Github</p>
               <p>HTML5</p>
+              <p>Java</p>
               <p>JavaScript</p>
               <p>Jest</p>
               <p>Material UI</p>
@@ -73,19 +90,30 @@ const About = () => {
               <p>Redux</p>
               <p>REST API</p>
               <p>Sequelize</p>
+              <p>SQL</p>
               <p>Tailwind</p>
               <p>TypeScript</p>
             </div>
           </div>
           <div className={classes.button}>
-            {tablet && <a target="_blank" rel="noreferrer" href={resumeLink}><Button size='md'>DOWNLOAD RESUME <i className="fa-solid fa-file-arrow-down"></i></Button></a>}
+            {tablet && (
+              <a target="_blank" rel="noreferrer" href={resumeLink}>
+                <Button size="md">
+                  DOWNLOAD RESUME{" "}
+                  <i className="fa-solid fa-file-arrow-down"></i>
+                </Button>
+              </a>
+            )}
           </div>
         </div>
         <div className={classes.button}>
-          {laptop &&
-            <a target="_blank" rel="noreferrer" href={`${resumeLink}`}><Button
-              size='md'>DOWNLOAD RESUME <i className="fa-solid fa-file-arrow-down"></i>
-            </Button></a>}
+          {laptop && (
+            <a target="_blank" rel="noreferrer" href={`${resumeLink}`}>
+              <Button size="md">
+                DOWNLOAD RESUME <i className="fa-solid fa-file-arrow-down"></i>
+              </Button>
+            </a>
+          )}
         </div>
       </div>
     </section>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,80 +1,111 @@
+import classes from "./Portfolio.module.scss";
 
-
-import classes from './Portfolio.module.scss';
-
-import Header from '../interface/Header';
-import Card from '../interface/Card';
+import Header from "../interface/Header";
+import Card from "../interface/Card";
 
 const Portfolio = () => {
-
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <div>
         <Header>PORTFOLIO</Header>
         <ul className={classes.content}>
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Ffilm-portfolio-screenshot.png?alt=media&token=796b8c1b-f4f2-4c40-a0e1-8eee5a7a884c'
-            title='Film Portfolio'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fhorse-race_gif.gif?alt=media&token=29e87d23-e313-44bf-8fc5-a6cddb9c5e0a"
+            title="Horse Race"
+            description="Horse Race is a web implementation of a drinking game usually played with a deck of cards. Players bet on up to four horses (represented as aces of a suit) and the horses themselves move across the 'field' based on cards that are drawn from the deck."
+            tech={["Emotion", "Next.js", "React.js", "Typescript", "Vercel"]}
+            git="https://github.com/mithintv/horse-race"
+            link="https://horse-race-mu.vercel.app/"
+          />
+          <Card
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Ffilm-portfolio-screenshot.png?alt=media&token=796b8c1b-f4f2-4c40-a0e1-8eee5a7a884c"
+            title="Film Portfolio"
             description="A web portfolio of my film and commercial work. Working on this portfolio was the initial inspiration and catalyst to my journey as a developer and software engineer."
-            tech={['Emotion', 'MySQL', 'Next.js', 'Sequelize', 'React.js', 'Typescript', 'Vercel']}
-            git='https://github.com/mithintv/film-portfolio'
-            link='https://mithin.tv'
+            tech={[
+              "Emotion",
+              "MySQL",
+              "Next.js",
+              "Sequelize",
+              "React.js",
+              "Typescript",
+              "Vercel",
+            ]}
+            git="https://github.com/mithintv/film-portfolio"
+            link="https://mithin.tv"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fono_1.gif?alt=media&token=017f895e-a683-4cf2-bf94-d33f8b1d8b7e'
-            title='One On One'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fono_1.gif?alt=media&token=017f895e-a683-4cf2-bf94-d33f8b1d8b7e"
+            title="One On One"
             description="One On One is a slack bot that randomly pairs up individuals within a channel for one-on-one's to help them get to know each other."
-            tech={['Jest', 'MongoDB', 'Node.js', 'Render', 'Slack API']}
-            git='https://github.com/mithintv/one-on-one'
-            link='https://slack.com/apps/A04DXH6FR8U-one-on-one'
+            tech={["Jest", "MongoDB", "Node.js", "Render", "Slack API"]}
+            git="https://github.com/mithintv/one-on-one"
+            link="https://slack.com/apps/A04DXH6FR8U-one-on-one"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fwordle-tracker-screen.png?alt=media&token=008c0cc0-0b22-4fce-825f-0eab63d487f4'
-            title='Wordle Leaderboard'
-            description='A slackbot that keeps track of daily Wordle scores hooked to a frontend that sorts players in a leaderboard with additional stats.'
-            tech={['Bulma', 'Express.js', 'MongoDB', 'Node.js', 'React.js', 'Render', 'Slack API',]}
-            git='https://github.com/mithintv/wordle-tracker'
-            link='https://wordle-tracker.netlify.app/#all'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fwordle-tracker-screen.png?alt=media&token=008c0cc0-0b22-4fce-825f-0eab63d487f4"
+            title="Wordle Leaderboard"
+            description="A slackbot that keeps track of daily Wordle scores hooked to a frontend that sorts players in a leaderboard with additional stats."
+            tech={[
+              "Bulma",
+              "Express.js",
+              "MongoDB",
+              "Node.js",
+              "React.js",
+              "Render",
+              "Slack API",
+            ]}
+            git="https://github.com/mithintv/wordle-tracker"
+            link="https://wordle-tracker.netlify.app/#all"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fportfolio-screen.png?alt=media&token=1e56e6d4-b07a-46a4-8359-e5ad66b29189'
-            title='Portfolio'
-            description='The latest iteration of my portfolio. This is a single-page built with React, CSS3/Sass and Firebase hosted on Netlify.'
-            tech={['CSS3/Sass', 'Firebase', 'Netlify', 'React']}
-            git='https://github.com/mithintv/portfolio-react'
-            link='https://portfolio-react-2022-v1.netlify.app/'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fportfolio-screen.png?alt=media&token=1e56e6d4-b07a-46a4-8359-e5ad66b29189"
+            title="Portfolio"
+            description="The latest iteration of my portfolio. This is a single-page built with React, CSS3/Sass and Firebase hosted on Netlify."
+            tech={["CSS3/Sass", "Firebase", "Netlify", "React"]}
+            git="https://github.com/mithintv/portfolio-react"
+            link="https://portfolio-react-2022-v1.netlify.app/"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Ffood-order-app-screen.png?alt=media&token=7c3b85d2-af31-4a6f-8ae7-2d50eaffc328'
-            title='Food Order App'
-            description='A front-end proof of concept with a dynamic cart for a hypothetical food ordering app built with React and Firebase.'
-            tech={['CSS3', 'Firebase', 'Netlify', 'React.js']}
-            git='https://github.com/mithintv/food-order-app'
-            link='https://papaya-lily-f123eb.netlify.app/'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Ffood-order-app-screen.png?alt=media&token=7c3b85d2-af31-4a6f-8ae7-2d50eaffc328"
+            title="Food Order App"
+            description="A front-end proof of concept with a dynamic cart for a hypothetical food ordering app built with React and Firebase."
+            tech={["CSS3", "Firebase", "Netlify", "React.js"]}
+            git="https://github.com/mithintv/food-order-app"
+            link="https://papaya-lily-f123eb.netlify.app/"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fkeeper-app-screen.png?alt=media&token=03f8b193-b2b2-4997-aba0-ed62ca110426'
-            title='Keeper'
-            description='A clone of Google Keep. Create, update, and delete notes!'
-            tech={['CSS3', 'Netlify', 'React.js']}
-            git='https://github.com/mithintv/keeper-app'
-            link='https://keeper-app-project.netlify.app/'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fkeeper-app-screen.png?alt=media&token=03f8b193-b2b2-4997-aba0-ed62ca110426"
+            title="Keeper"
+            description="A clone of Google Keep. Create, update, and delete notes!"
+            tech={["CSS3", "Netlify", "React.js"]}
+            git="https://github.com/mithintv/keeper-app"
+            link="https://keeper-app-project.netlify.app/"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fyelp-camp-screen.png?alt=media&token=54ae09d3-68db-4abb-9482-6d39418222fa'
-            title='Yelp Camp'
-            description='A Yelp-like application for campgrounds. Create an account and add campgrounds you have visited as well as review campgrounds that other users have added.'
-            tech={['Bootstrap', 'Cyclic', 'EJS', 'Express.js', 'Helmet', 'Node.js', 'MongoDB', 'Mongoose', 'Passport.js']}
-            git='https://github.com/mithintv/yelp-camp'
-            link='https://blush-nightingale-wear.cyclic.app/.netlify/functions/app/campgrounds'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fyelp-camp-screen.png?alt=media&token=54ae09d3-68db-4abb-9482-6d39418222fa"
+            title="Yelp Camp"
+            description="A Yelp-like application for campgrounds. Create an account and add campgrounds you have visited as well as review campgrounds that other users have added."
+            tech={[
+              "Bootstrap",
+              "Cyclic",
+              "EJS",
+              "Express.js",
+              "Helmet",
+              "Node.js",
+              "MongoDB",
+              "Mongoose",
+              "Passport.js",
+            ]}
+            git="https://github.com/mithintv/yelp-camp"
+            link="https://blush-nightingale-wear.cyclic.app/.netlify/functions/app/campgrounds"
           />
           <Card
-            img='https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fsimon-game-screen.png?alt=media&token=1d36f31e-7639-413c-b6d6-dbdc745705f8'
-            title='Simon Game'
-            description='This is a web implementation of Simon, an electronic game of short-term memory skill originally released in 1978 built with vanilla JavaScript and CSS3.'
-            tech={['CSS3', 'GitPages', 'HTML5', 'JavaScript']}
-            git='https://github.com/mithintv/simon-game'
-            link='https://mithintv.github.io/simon-game/'
+            img="https://firebasestorage.googleapis.com/v0/b/portfolio-react-5481e.appspot.com/o/images%2Fsimon-game-screen.png?alt=media&token=1d36f31e-7639-413c-b6d6-dbdc745705f8"
+            title="Simon Game"
+            description="This is a web implementation of Simon, an electronic game of short-term memory skill originally released in 1978 built with vanilla JavaScript and CSS3."
+            tech={["CSS3", "GitPages", "HTML5", "JavaScript"]}
+            git="https://github.com/mithintv/simon-game"
+            link="https://mithintv.github.io/simon-game/"
           />
         </ul>
       </div>
