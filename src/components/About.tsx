dc1944eeeaa150bee433d1d21526e1fb87bc3f70
Mithin Thomas <mithingthomas@gmail.com>
2025-05-16 13:21:38 -0400

    fix form mobile styles and remove resume

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,35 +1,7 @@
-import { useEffect, useState } from "react";
-
 import Header from "../ui/Header";
 import classes from "./About.module.scss";
-import Resume from "./Resume";
 
 const About = () => {
-	const [tablet, setTablet] = useState(false);
-	const [laptop, setLaptop] = useState(false);
-
-	const handleResize = () => {
-		if (window.innerWidth <= 1024) {
-			setLaptop(true);
-		} else if (window.innerWidth <= 768) {
-			setTablet(true);
-		} else {
-			setLaptop(false);
-			setTablet(false);
-		}
-	};
-
-	useEffect(() => {
-		window.addEventListener("resize", handleResize);
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
-
-	useEffect(() => {
-		handleResize();
-	});
-
 	return (
 		<section id="about">
 			<div className={classes.about}>
@@ -38,17 +10,16 @@ const About = () => {
 					<div className={classes.background}>
 						<h3>Background</h3>
 						<p className={classes.text}>
-							I am currently a Software Engineer splitting my time
-							between work and personal projects. I am passionate about
-							making the world a more egalitarian place and frequently think
-							about ideas and solutions that can make that world a reality.
+							I am currently a Software Engineer splitting my time between work
+							and personal projects. I am passionate about making the world a
+							more egalitarian place and frequently think about ideas and
+							solutions that can make that world a reality.
 						</p>
 						<p className={classes.text}>
 							When I am not coding, I am either writing a new screenplay or
 							actively working on a film/commerical set. You can check out my
 							film and production work <a href="https://mithin.tv">here</a>.
 						</p>
-						{!tablet && !laptop && <Resume />}
 					</div>
 					<div className={classes.skills}>
 						<h3>Technologies</h3>
@@ -91,9 +62,7 @@ const About = () => {
 							<p>TypeScript</p>
 						</div>
 					</div>
-					<div className={classes.button}>{tablet && <Resume />}</div>
 				</div>
-				<div className={classes.button}>{laptop && <Resume />}</div>
 			</div>
 		</section>
 	);
