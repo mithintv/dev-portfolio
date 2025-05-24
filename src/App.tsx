dc55358b5f6fd97c0e1b1c0436e9bd0dfc2c7237
Mithin Thomas <mithingthomas@gmail.com>
2025-05-24 17:05:50 -0400

    update socials

--- src/App.tsx
+++ src/App.tsx
@@ -40,13 +40,10 @@ const App = () => {
 			{sideBar && <Contact />}
 			{sideBar && (
 				<SideBar side="left">
-					<Social name="instagram" href="https://instagram.com/mithintv">
-						<i className="fa-brands fa-instagram"></i>
-					</Social>
 					<Social name="github" href="https://github.com/mithintv">
 						<i className="fa-brands fa-github"></i>
 					</Social>
-					<Social name="linkedin" href="https://linkedin.com/in/mithintv">
+					<Social name="linkedin" href="https://linkedin.com/in/mithin-thomas">
 						<i className="fa-brands fa-linkedin-in"></i>
 					</Social>
 				</SideBar>
